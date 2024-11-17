264701bee671b7397d190f84e2a9ecaf33868899
Bew-P <109616387+Bew-P@users.noreply.github.com>
2024-11-17 13:59:16 +0000

    commit test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -7,8 +7,7 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
-        
+        self.assertEqual(self.calc.add(1, 2), 3)        
 
     # Add the following test methods to the TestCalculator class:
     def test_add2(self):
